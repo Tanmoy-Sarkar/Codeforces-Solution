0a7af46bb375cf43ece46d57d6d6450ceab35671
Tanmoy-Sarkar <tanmoy9920@gmail.com>
2021-05-24 23:30:45 +0600

    58A

--- 58A - Chat room.py
+++ 58A - Chat room.py
@@ -4,9 +4,12 @@ given_string = input()
 
 try:
     result = (re.search('h(.*)o', given_string)).group(1)
-else:
-    pass
-try:
     result2 = (re.search('e(.*)l', result)).group(1)
-else:
-print(result2)
+    if "l" in result2:
+        print("YES")
+    else:
+        print("NO")
+    
+except:
+    print("NO")
+    
